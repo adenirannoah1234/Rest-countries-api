44be6db03c5f88b28f57251f015d14be4e9b19ad
adenirannoah1234 <adenirannoah9@gmail.com>
2023-10-22 14:01:33 +0100

    initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/navbar/Navbar';
 import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Details from './pages/detail/Details';
+import NotFound from './pages/notfound/NotFound';
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -22,6 +23,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details/:name" element={<Details />} />
+          <Route component={NotFound} />
         </Routes>
       </BrowserRouter>
     </div>
